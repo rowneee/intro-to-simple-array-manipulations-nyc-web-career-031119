3bc47ad565ffc25d6c53edc1778986433121a604
Roni Shabo <roni.shabo@gmail.com>
2019-02-25 22:20:21 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,43 @@
+def using_push(array, string)
+  return array.push(string)
+end
+
+def using_unshift(array, string)
+  return array.unshift(string)
+end
+
+def using_pop(array)
+  return array.pop
+end
+
+def pop_with_args(array)
+  return array.pop(2)
+end 
+
+def using_shift(array)
+  return array.shift
+end  
+
+def shift_with_args(array)
+  return array.shift(0..1)
+end 
+
+# def using_concat(array1, array2)
+#  return new_array = "#{array1}" + "#{array2}"
+#end
+
+# def using_insert(array, element)
+#   return array = array.insert(4, element)
+# end
+
+# def using_uniq(array)
+#  return array = array.uniq
+# end
+
+# def using_flatten(array)
+#   return array = array.flatten
+# end
+
+# def using.delete(array, string)
+#   return array = array.delete
+# end
