6af69062f4a4eb33e9514c4e0437add8599cf2ce
Roni Shabo <roni.shabo@gmail.com>
2019-02-25 22:31:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -32,9 +32,9 @@ def using_insert(array, element)
   return array.insert(4, element)
 end
 
-# def using_uniq(array)
-#  return array = array.uniq
-# end
+def using_uniq(array)
+  return array = array.uniq
+end
 
 # def using_flatten(array)
 #   return array = array.flatten
